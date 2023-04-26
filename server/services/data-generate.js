9207b26a8c03f89df9f94922933d931e113da9a2
SAK74 <sergijks@gmail.com>
2023-04-26 18:55:03 +0200

    feat: create simply flyght-data

---
+++ server/services/data-generate.js
@@ -0,0 +1,50 @@
+const cities = [
+  "Warsaw",
+  "Dublin",
+  "Tokyo",
+  "New York City",
+  "Beijing",
+  "London",
+  "Paris",
+  "Seoul",
+  "Dubai",
+  "Singapore",
+  "Barcelona",
+  "Istanbul",
+  "Los Angeles",
+  "Rome",
+  "Shanghai",
+  "Chicago",
+  "Berlin",
+  "Hong Kong",
+  "Sydney",
+  "Amsterdam",
+  "San Francisco",
+];
+function cityGenerate(prev) {
+  const tempGenerate = cities[Math.round(Math.random() * 21)];
+  return tempGenerate !== prev ? tempGenerate : cityGenerate(prev);
+}
+
+function dateGener() {
+  const month = Math.round(Math.random() * 5) + 6;
+  const day = Math.round(Math.random() * 30) + 1;
+  return new Date(2023, month, day).toDateString();
+}
+
+const data = [];
+function generate(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const fromCity = cityGenerate();
+    data.push({
+      from: fromCity,
+      to: cityGenerate(fromCity),
+      date: dateGener(),
+      available_seats: Math.round(Math.random() * 250),
+      cost: Math.round(Math.random() * 1000) + 200,
+    });
+  }
+  return data;
+}
+
+console.log(JSON.stringify(generate(100)));
