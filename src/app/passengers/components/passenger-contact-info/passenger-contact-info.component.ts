7129cb184df5cec29b50b82a5c242f004ead58ba
SAK74 <sergijks@gmail.com>
2023-05-09 20:54:30 +0200

    feat: add conntect info to passeng form

--- src/app/passengers/components/passenger-contact-info/passenger-contact-info.component.ts
+++ src/app/passengers/components/passenger-contact-info/passenger-contact-info.component.ts
@@ -1,12 +1,5 @@
-import { Component } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  AbstractControl,
-  ValidationErrors,
-  FormGroupDirective,
-} from '@angular/forms';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-passenger-contact-info',
@@ -14,50 +7,28 @@ import {
   styleUrls: ['./passenger-contact-info.component.css'],
 })
 export class PassengerContactInfoComponent {
-  contactForm!: FormGroup<{
-    email: FormControl<string>;
-  }>;
+  @Input() control!: FormGroup;
   fieldRequired: string = 'This field is required';
+
   ngOnInit(): void {
-    this.createForm();
-  }
-  createForm() {
-    let emailregex: RegExp =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    this.contactForm = new FormGroup({
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.pattern(emailregex)],
+    this.control.addControl(
+      'email',
+      new FormControl('', {
+        validators: [Validators.required, Validators.email],
         nonNullable: true,
-      }),
-    });
+      })
+    );
   }
+
   emaiErrors() {
-    return this.contactForm.get('email')?.hasError('required')
+    return this.control.get('email')?.hasError('required')
       ? 'This field is required'
-      : this.contactForm.get('email')?.hasError('pattern')
+      : this.control.get('email')?.hasError('email')
       ? 'Not a valid emailaddress'
       : '';
   }
+
   checkValidation(input: string) {
-    const validation =
-      this.contactForm.get(input)?.invalid &&
-      (this.contactForm.get(input)?.dirty ||
-        this.contactForm.get(input)?.touched);
-    return validation;
-  }
-
-  onSubmit(formDirective: FormGroupDirective): void {
-    console.log(formDirective);
-
-    // const email = formData.value.email;
-    // const password = formData.value.password;
-    // const username = formData.value.username;
-    // const { email, password, username } = this.registerForm.value;
-    // console.log(formDirective, password);
-    // this.auth.registerUser(email!, password!).subscribe((test) => {
-    //   console.log(test);
-    //   formDirective.resetForm();
-    //   this.registerForm.reset();
-    // });
+    return this.control.get(input)?.invalid;
   }
 }
