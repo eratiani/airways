91a83e5aa69d69dbe87d346188fb358d450e2573
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-05-15 11:13:17 +0400

    minor fixes to summary data

--- src/app/summary/flight-details/flight-details.component.ts
+++ src/app/summary/flight-details/flight-details.component.ts
@@ -1,27 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FlightDataType, ReservationDataType } from 'src/app/models/flyght-data.model';
+import { Component, Input } from '@angular/core';
+import { FlightDataType } from 'src/app/models/flyght-data.model';
 
 @Component({
   selector: 'app-flight-details',
   templateUrl: './flight-details.component.html',
   styleUrls: ['./flight-details.component.css'],
 })
-export class FlightDetailsComponent implements OnInit {
-  @Input() flightDetails!: any ;
+export class FlightDetailsComponent {
+  @Input() flightDetails!: any;
   @Input() oneWayFlight?: FlightDataType;
   @Input() backFlight?: FlightDataType;
-  timeOfLanding(flightObj:FlightDataType) {
+  timeOfLanding(flightObj: FlightDataType) {
     const randomHours = Math.floor(Math.random() * 12) + 1;
     const date = new Date(flightObj.date);
-    date.setHours(date.getHours() + randomHours); 
-    // const formattedDate = date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }); // formats the date as "Sep 8, 2023"
-    
-    console.log(date,flightObj);
-    
+    date.setHours(date.getHours() + randomHours);
+
     return date;
   }
-  ngOnInit(): void {
-    console.log(this.flightDetails);
-  }
-
 }
--- src/app/summary/flight-fare/flight-fare.component.ts
+++ src/app/summary/flight-fare/flight-fare.component.ts
@@ -1,35 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import {
-  FlightDataType,
-  ReservationDataType,
-} from 'src/app/models/flyght-data.model';
-import { StoreType } from 'src/app/redux/store.model';
+import { HeaderStateService } from 'src/app/core/services/header-state.service';
+import { FlightDataType } from 'src/app/models/flyght-data.model';
 
 @Component({
   selector: 'app-flight-fare',
   templateUrl: './flight-fare.component.html',
   styleUrls: ['./flight-fare.component.css'],
 })
-export class FlightFareComponent implements OnInit {
+export class FlightFareComponent {
   @Input() oneWayFlight?: FlightDataType;
   @Input() backFlight?: FlightDataType;
   @Input() flightDetails!: any;
-  passengerCount!: any;
-  constructor(private store: Store<StoreType>) {}
-  ngOnInit(): void {
-    this.store
-      .select('passengersCount')
-      .subscribe((data) => (this.passengerCount = data));
-    console.log(this.passengerCount);
-  }
+  constructor(public state: HeaderStateService) {}
+
   totalPrice() {
     if (!this.oneWayFlight) return;
     let priceSum = 0;
-    const prices = document.querySelectorAll('.price__Ticket').forEach((e) => {
-      priceSum += Number(e.textContent);
-    });
 
+    const prices = document.querySelectorAll('.price__Ticket').forEach((e) => {
+      if (e.textContent) {
+        priceSum += Number(e.textContent.replace(/\D/g, '')) / 100;
+      }
+    });
     return priceSum;
   }
 }
