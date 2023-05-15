2705b9401ec08691d24a651ceee9dcf7500272f2
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-05-15 09:36:35 +0400

    fix total price

--- src/app/summary/flight-fare/flight-fare.component.ts
+++ src/app/summary/flight-fare/flight-fare.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { FlightDataType, ReservationDataType } from 'src/app/models/flyght-data.model';
+import {
+  FlightDataType,
+  ReservationDataType,
+} from 'src/app/models/flyght-data.model';
 import { StoreType } from 'src/app/redux/store.model';
 
 @Component({
@@ -8,33 +11,25 @@ import { StoreType } from 'src/app/redux/store.model';
   templateUrl: './flight-fare.component.html',
   styleUrls: ['./flight-fare.component.css'],
 })
-export class FlightFareComponent implements OnInit{
+export class FlightFareComponent implements OnInit {
   @Input() oneWayFlight?: FlightDataType;
   @Input() backFlight?: FlightDataType;
-  @Input() flightDetails!: any ;
-  passengerCount!:any
-constructor(private store: Store<StoreType>){}
+  @Input() flightDetails!: any;
+  passengerCount!: any;
+  constructor(private store: Store<StoreType>) {}
   ngOnInit(): void {
     this.store
       .select('passengersCount')
       .subscribe((data) => (this.passengerCount = data));
     console.log(this.passengerCount);
-    
   }
-  totalPrice(){
-    if(!this.oneWayFlight ) return
-    let total = 0;
-    if (this.passengerCount) {
-       total = (this.oneWayFlight.cost * this.passengerCount?.adult) + 
-                    (this.oneWayFlight.cost * this.passengerCount?.child) + 
-                    (this.oneWayFlight.cost * this.passengerCount?.infant);
-    }
-    if(this.backFlight) {
-      total += (this.backFlight.cost * this.passengerCount?.adult) + 
-                    (this.backFlight.cost * this.passengerCount?.child) + 
-                    (this.backFlight.cost * this.passengerCount?.infant);
-    }
-    return total;
+  totalPrice() {
+    if (!this.oneWayFlight) return;
+    let priceSum = 0;
+    const prices = document.querySelectorAll('.price__Ticket').forEach((e) => {
+      priceSum += Number(e.textContent);
+    });
+
+    return priceSum;
   }
-  
 }
