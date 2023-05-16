d491ccde6a4a0dd7b9dca381556dc687190094a2
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-05-17 00:21:19 +0400

    style 404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css'],
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  constructor(private router:Router, private route:ActivatedRoute){}
+  onNavigateBack(): void {
+    this.router.navigate(['../'],{ relativeTo: this.route });
+  }
+}
